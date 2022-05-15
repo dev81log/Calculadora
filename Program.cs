8ddf256c5a594fb29ac62ac6cb11d2e46f0cb22c
dev81log <dev81log@gmail.com>
2022-05-15 17:33:31 -0300

    Programa simples de calcular

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate();
+        }
+
+        static void Calculate()
+        {
+            Console.Clear();
+            Console.WriteLine("Escolha uma das opcoes:");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtracao");
+            Console.WriteLine("3 - Divisao");
+            Console.WriteLine("4 - Multiplicacao");
+            Console.WriteLine("5 - Sair");
+
+            Console.WriteLine("---------------------");
+            short opcoesMenu = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o 1 valor:");
+            float valorEntrada1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o 2 valor:");
+            float valorEntrada2 = float.Parse(Console.ReadLine());
+
+            float itemArit = 0;
+
+            switch (opcoesMenu)
+            {
+                case 1:
+                    itemArit = valorEntrada1 + valorEntrada2;
+                    Console.WriteLine($"O resultado da Soma: {itemArit}");
+                    break;
+                case 2:
+                    itemArit = valorEntrada1 - valorEntrada2;
+                    Console.WriteLine($"O resultado da Subtracao {itemArit}");
+                    break;
+                case 3:
+                    itemArit = valorEntrada1 / valorEntrada2;
+                    Console.WriteLine($"O resultado da Divisao {itemArit}");
+                    break;
+                case 4:
+                    itemArit = valorEntrada1 * valorEntrada2;
+                    Console.WriteLine($"O resultado da Multiplicacao {itemArit}");
+                    break;
+                case 5: System.Environment.Exit(0); break;
+                default:
+                    break;
+            }
+        }
+    }
+}
